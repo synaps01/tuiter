f58d5b4495a46a3e710201c7954b4e1173f0821c
Alejandro Lozano <alexlozanob@gmail.com>
2016-01-03 14:26:45 -0600

    view for profile and edit profile

--- tuiter/tuits/urls.py
+++ tuiter/tuits/urls.py
@@ -7,6 +7,7 @@ from .views import tuits
 urlpatterns = [
     url(r'^$', tuits.landingPageView, name='landing'),
     url(r'login$', tuits.tuiter_login, name='login'),
+    url(r'register$', tuits.register, name='register'),
     url(r'logout$', tuits.tuiter_logout, name='logout'),
     url(r'timeline$', tuits.timeline, name='timeline'),
     url(r'newtuit$', tuits.newtuit, name='newtuit'),
